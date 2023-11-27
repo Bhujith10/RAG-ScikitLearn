0690d6d3524b2a6ee02be1a52e9d95c42a8f9234
Bhujith Madav Velmurugan <65334107+Bhujith10@users.noreply.github.com>
2023-11-27 13:01:16 +0530

    Uploded preprocessing files

---
+++ utils.py
@@ -0,0 +1,10 @@
+import tiktoken
+
+def get_num_tokens(text):
+    enc = tiktoken.get_encoding("cl100k_base")
+    return len(enc.encode(text))
+
+
+def trim(text, max_context_length):
+    enc = tiktoken.get_encoding("cl100k_base")
+    return enc.decode(enc.encode(text)[:max_context_length])
